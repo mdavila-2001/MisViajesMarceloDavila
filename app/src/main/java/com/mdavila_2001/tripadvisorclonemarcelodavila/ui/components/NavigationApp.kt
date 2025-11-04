1d11feb631a22e73cb8c2dcb0ea2008bc619dfb7
marcelofos <dmarcelo201@gmail.com>
2025-11-04 15:00:36 -0400

    Added a Login Screen

--- app/src/main/java/com/mdavila_2001/tripadvisorclonemarcelodavila/ui/components/NavigationApp.kt
+++ app/src/main/java/com/mdavila_2001/tripadvisorclonemarcelodavila/ui/components/NavigationApp.kt
@@ -1,10 +1,12 @@
 package com.mdavila_2001.tripadvisorclonemarcelodavila.ui.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.NavRoutes
+import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.screens.LoginScreen
 import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.screens.SplashScreen
 import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.viewmodels.SplashViewModel
 
@@ -22,7 +24,10 @@ fun NavigationApp() {
             )
         }
         composable(NavRoutes.Login.route) {
-            //LoginScreen(navController = navController)
+            LoginScreen(
+                navController = navController,
+                modifier = Modifier
+            )
         }
         composable(NavRoutes.Trips.route) {
             //TripsScreen(navController = navController)
--- app/src/main/java/com/mdavila_2001/tripadvisorclonemarcelodavila/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/mdavila_2001/tripadvisorclonemarcelodavila/ui/screens/LoginScreen.kt
@@ -1,21 +1,117 @@
 package com.mdavila_2001.tripadvisorclonemarcelodavila.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.mdavila_2001.tripadvisorclonemarcelodavila.R
+import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.theme.TripAdvisorCloneMarceloDavilaTheme
 import com.mdavila_2001.tripadvisorclonemarcelodavila.ui.viewmodels.LoginViewModel
+
 
 @Composable
 fun LoginScreen(
     navController: NavController,
     viewModel: LoginViewModel = viewModel(),
     modifier: Modifier,
-) {}
+) {
+    var userName by rememberSaveable { mutableStateOf("") }
+
+    val navigate by viewModel.navigateToMain.collectAsState()
+
+    LaunchedEffect(navigate) {
+        if (navigate) {
+            navController.navigate("trips") {
+                popUpTo("login") { inclusive = true }
+            }
+            viewModel.onNavigationDone()
+        }
+    }
+
+    LoginView(
+        userName = userName,
+        onUserNameChanged = { userName = it },
+        onLoginClick = { viewModel.onLoginClicked(userName) }
+    )
+}
 
 @Composable
 private fun LoginView(
     userName: String,
     onUserNameChanged: (String) -> Unit,
     onLoginClick: () -> Unit,
-) {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.splash),
+            contentDescription = "Logo de la App",
+            modifier = Modifier.size(180.dp)
+        )
+        Spacer(modifier = Modifier.size(32.dp))
+
+        Text(
+            text = "Ingrese su usuario:",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value= userName,
+            onValueChange = onUserNameChanged,
+            placeholder = { Text("Nombre de Usuario") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = onLoginClick,
+            enabled = userName.isNotBlank(),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Iniciar Sesión")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    TripAdvisorCloneMarceloDavilaTheme {
+        LoginView(
+            userName = "",
+            onUserNameChanged = {},
+            onLoginClick = {}
+        )
+    }
+}
